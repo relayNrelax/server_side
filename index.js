f91a7661034f4d6fcc112a318e78561e4eb65e0f
Bishal Deb <102280193+bishald0013@users.noreply.github.com>
2023-10-14 22:49:31 +0530

    index file changed

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/config.js';
 import userRouter from './route/userRoute.js';
-import cors from "cors"
+import cors from "cors";
 import alertRoute from './route/alertRoute.js';
-dotenv.config()
 
-const app = express()
+dotenv.config();
+
+const app = express();
 app.use(cors({
     origin: 'https://relynrelax.com/',
-}))
+}));
 
-const port = 5001
+const port = 5001;
 
-connectDB()
-app.use(express.json({ limit: "30mb", extended: true }))
-app.use(express.json());
+(async () => {
+    try {
+        await connectDB(); // Establish the MongoDB connection
+        app.use(express.json({ limit: "30mb", extended: true }));
+        app.use(express.json());
 
-app.use('/api/user', userRouter)
-app.use('/api/alert', alertRoute)
+        app.use('/api/user', userRouter);
+        app.use('/api/alert', alertRoute);
 
-app.listen(port, () => {
-    console.log(`Server is listening on ${port}`);
-})
+        app.listen(port, () => {
+            console.log(`Server is listening on ${port}`);
+        });
+    } catch (error) {
+        console.error('Failed to connect to the database:', error);
+    }
+})();
