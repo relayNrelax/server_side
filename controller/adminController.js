b5a098e4e8f79d54d5d6dc820c1982b44223cc5f
bishald0013 <bishal986473@gmail.com>
2024-03-15 00:13:30 +0530

    user details also added

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,5 +1,6 @@
 import AlertService from "../service/alertService.js";
 import AdminService from "../service/adminService.js";
+import UserModel from "../models/userModel.js";
 
 export default class AdminController {
     constructor(){
@@ -19,7 +20,6 @@ export default class AdminController {
     getUsers = async (req, res) => {
         try {
             const userData = await this.AdminService.getAllUsers()
-            console.log(userData);
             res.send(userData) 
         } catch (error) {
             res.send(error.message);
@@ -29,7 +29,30 @@ export default class AdminController {
     getAlerts = async (req, res) => {
         try {
             const alertData = await this.AdminService.alerts()
-            res.send(alertData)
+            const userId = alertData.map((data) => data.a_u_id);
+            const userData = await UserModel.find({
+                '_id': { $in: userId }
+            })
+            // console.log(userData);
+            const alertsWithUserData = alertData.map((alert) => {
+                const user = userData.find((user) => user._id.toString() === alert.a_u_id.toString());
+                return {
+                        _id: alert._id,
+                        a_name: alert.a_name,
+                        a_type: alert.a_type,
+                        a_v_number: alert.a_v_number,
+                        a_status: alert.a_status,
+                        a_u_id: alert.a_u_id,
+                        a_end_date: alert.a_end_date,
+                        a_created_by: alert.a_created_by,
+                        userPhone: user ? user.phone_number : null,
+                        userAltNumber: user ? user.alternate_number : null,
+                        userEmail : user ? user.email : null,
+                        __v: alert.__v
+                    }
+            })
+            // console.log(alertsWithUserData);
+            res.status(200).send(alertsWithUserData);
         } catch (error) {
             res.send(error.message)
         }
